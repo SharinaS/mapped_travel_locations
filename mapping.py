23fbb213af4bf0f44baa1419770e78d69f353034
Sharina Stubbs <sharinastubbs@gmail.com>
2018-08-02 13:49:54 -0700

    Initial commit

---
+++ mapping.py
@@ -0,0 +1,82 @@
+'''
+Created on Jul 29, 2018
+
+@author: sharina stubbs
+
+Project description: A web-based mapping project of the places I've visited, with interactive pop-ups and color coding
+to identify number of times a place has been visited. Project uses folium, pandas, python; csv is used to store the data.
+Note - dataframes converted into python lists to improve speed of program.
+
+Version: 1.0
+
+'''
+
+
+import folium
+import pandas
+
+
+data = pandas.read_csv("places_visited.txt")
+lat = list(data["LATITUDE"])
+lon = list(data["LONGITUDE"])
+des = list(data["DESTINATION"])
+vis = list(data["VISITS"])
+
+
+def visit_times(visits):
+    if visits < 2:
+        return 'green'
+    elif 2 <= visits < 6:
+        return 'blue'
+    elif 6 <= visits < 11:
+        return 'yellow'
+    elif 11 <= visits < 16:
+        return 'orange'
+    else:
+        return 'red' #for 16 and upward
+    
+
+base_map = folium.Map(location=[48.864716,2.349014], zoom_start=2)
+
+fg = folium.FeatureGroup(name="Travel Map")
+fg.add_child(folium.Marker(location=[47.608013, -122.335167], popup="Home Base", icon=folium.Icon(color='pink')))
+
+for lt, ln, ds, vs in zip(lat,lon,des,vis):
+    if vs <2:
+        fg.add_child(folium.Marker(location=[lt,ln], popup="Visited " + str(vs) + " time", icon=folium.Icon(color=visit_times(vs))))
+    else:
+        fg.add_child(folium.Marker(location=[lt,ln], popup="Visited " + str(vs) + " times", icon=folium.Icon(color=visit_times(vs))))
+        
+
+base_map.add_child(fg)
+
+base_map.save("Map of Explorations.html")
+
+
+
+
+#===============================================================================
+# print(data)
+# print(data.columns)
+# print(lat)
+# print(lon)
+# print(des)
+# print(help(folium.Marker))
+#===============================================================================
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
